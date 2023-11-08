c98c4c028092f74d31aae4f6b3b0fe026389bb18
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-11-08 18:03:51 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    bodyElem: document.querySelector('body')
+};
 
+    refs.startBtn.addEventListener('click', onStartBtnClick);
+    refs.stopBtn.addEventListener('click', onStopBtnClick);
+    
+  function onStartBtnClick(event) {
+      colorSwitcher.start();
+}
+  function onStopBtnClick(event) {
+    colorSwitcher.stop();
+}
+
+const colorSwitcher = {
+    setIntervalId: null,
+
+    start() {
+        refs.startBtn.disabled = true;
+        refs.stopBtn.disabled = false;
+
+        this.setIntervalId = setInterval(() => {
+      this.change();
+    }, 1000);
+    },
+
+    stop() {
+        clearInterval(this.setIntervalId);
+        refs.stopBtn.disabled = true;
+        refs.startBtn.disabled = false;
+    },
+
+    change() {
+        refs.bodyElem.style.backgroundColor = getRandomHexColor();
+    }
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
